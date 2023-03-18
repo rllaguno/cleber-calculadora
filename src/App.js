dec93fdb443ee2473eb69eb25da00dad38c6bfd7
Rodrigo Llaguno Cardenas <rodrigollagunoc@gmail.com>
2023-03-18 13:04:39 -0600

    CSS Update

--- src/App.js
+++ src/App.js
@@ -1,20 +1,51 @@
-import logo from './logo.svg';
+import * as React from 'react';
 import './App.css';
 
 function App() {
+  const options = [
+    {label: '+', value: '+'},
+    {label: '-', value: '-'},
+    {label: '*', value: '*'},
+    {label: '/', value: '/'},
+  ]
+  
+  const [value, setValue] = React.useState('+');
+
+  const handleChange = (event) => {
+ 
+    setValue(event.target.value);
+  };
+
   return (
     <div className="App">
       <header className="App-header">
         <a className='App-title'>
-          Calculator
+          Calculadora Cleber
         </a>
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        
       </header>
+      <input type='text' className='App-rect App-rect1' placeholder='Num 1'></input>
+
+       <select value={value} onChange={handleChange} className='App-square ' >
+         {options.map((option) => (
+           <option value={option.value}>{option.label}</option>
+         ))}
+       </select>
+
+       <input type='text' className='App-rect App-rect2' placeholder='Num 2'></input>
+       <a className='App-equal'>=</a>
+       <output  className='App-output' ></output>
+
+       <div className='App-rect-line App-rect1'></div>
+       <div className='App-square-line'></div>
+       <div className='App-rect-line App-rect2'></div>
+       <div className='App-rect-line App-rect3'></div>
+     
+    
+
+
     </div>
-  );
+      );
 }
 
 export default App;
